8b5c1aca9685f6dc736dfb50d4f93667cac2fd4d
Fernando Tóffolo <fmtoffolo@gmail.com>
2014-01-31 11:31:11 -0300

    First
    Just a first hack at 3 am. Don't judge me

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var photo = document.getElementsByClassName('photo')[0],
+	caption = document.getElementsByClassName('caption')[0],
+	text = document.getElementById('text'),
+	photoList = ['fondo.jpg', 'fondo2.jpg', 'fondo3.jpg', 'fondo4.jpg'];
+
+//Set listener for device orientation.
+window.addEventListener('deviceorientation', handleMotion, true);
+
+photo.style.backgroundImage = 'url("img/' + photoList[Math.floor(Math.random()*photoList.length)] + '")' 
+
+function handleMotion(data){
+	var orientation = {x : data.gamma + 90, y : data.beta - 90, z : data.alpha};
+	// photo.innerHTML = orientation.x;
+	if (orientation.x > 100) {
+		caption.classList.add('fadeOut');
+		photo.classList.remove('moveRight', 'center');
+		photo.classList.add('moveLeft');
+		text.innerHTML = photo.classList;
+	}else if(orientation.x < 80) {
+		caption.classList.add('fadeOut');
+		photo.classList.remove('moveLeft', 'center');
+		photo.classList.add('moveRight');
+		text.innerHTML = photo.classList;
+	}else{
+		caption.classList.remove('fadeOut');
+		photo.classList.remove('moveRight', 'moveLeft');
+		photo.classList.add('center');
+		text.innerHTML = photo.classList;
+	};
+}
+
