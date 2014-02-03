40f840304e77edd21759f8e763da6fca8a365ac9
Fernando Tóffolo <fmtoffolo@gmail.com>
2014-02-03 14:53:25 -0300

    made it modular
    just chanegd somethings to make it modular

--- js/main.js
+++ js/main.js
@@ -1,37 +1,64 @@
-(function(){
+var PhotoPaper = function(config){
 
-var photo = document.getElementsByClassName('picture')[0],
-	caption = document.getElementsByClassName('caption')[0],
-	text = document.getElementById('text'),
-	photoList = ['fondo.jpg', 'fondo2.jpg', 'fondo3.jpg', 'fondo4.jpg'];
+	var photoContainer,
+		image,
+		caption, 
+		profile, 
+		text,
+		imageUrl = config.url || 'img/fondo3.jpg',
+		captionText = config.text || 'PhotoPaper demo',
+		profilePic = config.profilePic || 'img/profile.jpg';
 
-//Set listener for device orientation.
-window.addEventListener('deviceorientation', handleMotion, true);
+	
 
-photo.src = 'img/' + photoList[Math.floor(Math.random()*photoList.length)]; 
+	var render = function(){
+		photoContainer = document.createElement('div');
+		image = document.createElement('img');
+		caption = document.createElement('div');
+		profile = document.createElement('img');
+		text = document.createElement('p');
 
-function handleMotion(data){
-	var orientation = {x : data.gamma + 90, y : data.beta - 90, z : data.alpha};
+		document.body.appendChild(photoContainer).setAttribute('id', 'photoContainer');
+		photoContainer.appendChild(image).classList.add('picture');
+		photoContainer.appendChild(caption).classList.add('caption');
+		caption.appendChild(profile).classList.add('profile');
+		caption.appendChild(text);
 
-	if (orientation.x > 100) {
-		caption.classList.add('fadeOut');
-		photo.classList.remove('moveRight', 'center');
-		photo.classList.add('moveLeft');
-		text.innerHTML = photo.classList;
-		console.log(photo.classList);
-	}else if(orientation.x < 80) {
-		caption.classList.add('fadeOut');
-		photo.classList.remove('moveLeft', 'center');
-		photo.classList.add('moveRight');
-		text.innerHTML = photo.classList;
-		console.log(photo.classList);
-	}else{
-		caption.classList.remove('fadeOut');
-		photo.classList.remove('moveRight', 'moveLeft');
-		photo.classList.add('center');
-		text.innerHTML = photo.classList;
-		console.log(photo.classList);
-	};
-}
+		image.src = imageUrl;
+		profile.src = profilePic;
+		text.innerHTML = captionText;
 
-})();
+		//Set listener for device orientation.
+		window.addEventListener('deviceorientation', handleMotion, true);
+
+	} 
+
+	var handleMotion = function(data){
+		var orientation = {x : data.gamma + 90, y : data.beta - 90, z : data.alpha};
+
+		if (orientation.x > 100) {
+			caption.classList.add('fadeOut');
+			image.classList.remove('moveRight', 'center');
+			image.classList.add('moveLeft');
+			// text.innerHTML = image.classList;
+
+		}else if(orientation.x < 80) {
+			caption.classList.add('fadeOut');
+			image.classList.remove('moveLeft', 'center');
+			image.classList.add('moveRight');
+			// text.innerHTML = image.classList;
+
+		}else{
+			caption.classList.remove('fadeOut');
+			image.classList.remove('moveRight', 'moveLeft');
+			image.classList.add('center');
+			// text.innerHTML = image.classList;
+
+		};
+	}
+
+	render();
+
+};
+
+var photo = new PhotoPaper({});
